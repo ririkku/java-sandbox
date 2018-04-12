c5089016332b88ece6775d1c7a5c640668d485d5
kikukawa_ryoji <ryoji19950109@gmail.com>
2018-04-12 17:28:50 +0900

    テキストファイルの読み書きをして見た

--- src/main/java/kiku/file/FileSample.java
+++ src/main/java/kiku/file/FileSample.java
@@ -1,32 +1,33 @@
 package kiku.file;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileSample {
 
+    private String dirPath = String.join(File.separator, "src", "main", "java", "kiku", "file");
+    private String filePath = String.join(File.separator, "src", "main", "java", "kiku", "file", "fileSample.txt");
+
     public boolean ファイルが存在するか() {
-        String filePath = String.join(File.separator, "src", "main", "java", "kiku", "file", "fileSample.txt");
         Path path = Paths.get(filePath);
         File file = path.toFile();
         return file.exists();
     }
 
     public String[] ディレクトリ() {
-        String dirPath = String.join(File.separator, "src", "main", "java", "kiku", "file");
         File file = Paths.get(dirPath).toFile();
         return file.list();
     }
 
     public void バイナリファイルを読み込んでみる() {
-        String fileName = String.join(File.separator, "src", "main", "java", "kiku", "file", "fileSample.txt");
-        Path path = Paths.get(fileName);
+        Path path = Paths.get(filePath);
         try (InputStream stream = Files.newInputStream(path)) {
             for (int ch; (ch = stream.read()) != -1; ) {
                 System.out.println(String.format("%c : %s", (char) ch, Integer.toHexString(ch)));
@@ -38,14 +39,49 @@ public class FileSample {
     }
 
     public void バイナリファイルに書き込んでみる() {
-        String fileName = String.join(File.separator, "src", "main", "java", "kiku", "file", "fileSample.txt");
         byte[] data = new byte[]{0x41, 0x42, 0x43};
-        Path path = Paths.get(fileName);
+        Path path = Paths.get(filePath);
         try (OutputStream stream = Files.newOutputStream(path, StandardOpenOption.APPEND, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
             stream.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public void テキストファイルを読み込んでみる() {
+        Path path = Paths.get(filePath);
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void テキストファイルに書き込んでみる() {
+        Path path = Paths.get(filePath);
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            writer.write("hosomi test1 test2");
+            writer.newLine();
+            writer.write("ubukata test1 test2");
+            writer.newLine();
+            writer.write("takada test1 test2");
+            writer.newLine();
+            writer.write("takahashi test1 test2");
+            writer.newLine();
+            writer.write("hosomi test1 test2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> テキストファイルの人の部分を取得してみる() {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
+            return reader.lines().map(line -> line.split(" ")[0]).distinct().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 }
--- src/test/java/kiku/file/FileSampleTest.java
+++ src/test/java/kiku/file/FileSampleTest.java
@@ -3,6 +3,8 @@ package kiku.file;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileSampleTest {
@@ -34,5 +36,21 @@ public class FileSampleTest {
     @Test
     public void 書いてみる() {
         sut.バイナリファイルに書き込んでみる();
+    }
+
+    @Test
+    public void テキストファイルを読み込んでみる() {
+        sut.テキストファイルを読み込んでみる();
+    }
+
+    @Test
+    public void テキストファイルに書き込んでみる() {
+        sut.テキストファイルに書き込んでみる();
+    }
+
+    @Test
+    public void テキストファイルの特定文字列を取得してみる() {
+        List<String> actual = sut.テキストファイルの人の部分を取得してみる();
+        assertThat(actual).contains("hosomi", "ubukata", "takada", "takahashi");
     }
 }
